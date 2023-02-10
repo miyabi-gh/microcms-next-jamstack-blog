6a2e70f3c78f98be8999a73a38c1e05ffb45db8d
otsuka masashi <m_otsuka@irokoto.co.jp>
2023-02-10 13:26:54 +0900

    fourth commit

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,23 +1,24 @@
-import fetch from 'node-fetch';
+import { client } from "@pages/api/microcms";
 
 export default async (req, res) => {
   if (!req.query.slug) {
     return res.status(404).end();
   }
-  const content = await fetch(
-    `https://cgv61vn5t7.microcms.io/api/v1/blog/${req.query.slug}?fields=id&draftKey=${req.query.draftKey}`,
-    { headers: { 'X-MICROCMS-API-KEY': process.env.apiKey || '' } }
-  )
-  .then(res => res.json()).catch(error => null);
+  
+  const content = await client.get({
+    endpoint: 'blogs',
+    contentId: req.query.slug,
+    queries: {draftKey: req.query.draftKey}
+  }).then().catch(error => console.error(error));
 
   if (!content) {
     return res.status(401).json({ message: 'Invalid slug' });
   }
 
   res.setPreviewData({
-    slug: content.id,
+    slug: content["id"],
     draftKey: req.query.draftKey,
   });
-  res.writeHead(307, { Location: `/${content.id}` });
+  res.writeHead(307, { Location: `/blogs/${content["id"]}` });
   res.end('Preview mode enabled');
 };
